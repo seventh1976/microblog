fd2f345e07b894d8dc2548282b11985de26cc957
seventh1976 <seventh1976@gmail.com>
2018-02-15 12:26:27 +0800

    adding avatar from gravatar

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db
-from app import login
+from hashlib import md5
+
+
+from app import db, login
+
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -19,6 +22,10 @@ class User(UserMixin, db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return 'https://www.gravatar.com/avatar/{}?=identicon&s={}'.format(digest, size)
 
 
 class Post(db.Model):
